ed45d919d13b3074d9f13050ffb8f9fae90db985
Alexia <aoviedo70248@ufide.ac.cr>
2023-09-28 20:01:41 -0600

    Update
    Esto es una prueba

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,2 +1,5 @@
 console.log('hola mundo')
 este es un ejecicio de uso de git y github
+console.log('Alexia Oviedo')
+este es un ejecicio de uso de git y github
+este es un ejecicio de uso de git y github
